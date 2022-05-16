ff197ecdc4ef87097c76f8d1f11272c4475f8cc2
Kay'z.Part <particulier.flore@gmail.com>
2022-05-16 09:06:35 +0200

    verif + edit classes

--- Classes/Category.php
+++ Classes/Category.php
@@ -6,12 +6,11 @@
         public function getId_category(){
             return $this->_id_category;
         }
-        public function setId_category( $id_category){
-            $this->_id_category = $id_category;
-        }
-
         public function getName_category(){
             return $this->_name_category;
+        }
+        public function setId_category( $id_category){
+            $this->_id_category = $id_category;
         }
         public function setName_category($name_category){
             $this->_name_category = $name_category;
--- Classes/Reserv.php
+++ Classes/Reserv.php
@@ -1,44 +1,41 @@
 <?php 
 class Reserv extends Classes{
     private $_id_reserv;
-    private $_id_user;
     private $_id_book;
+    private $_id_user;
     private $_change_condition;
+    private $_date_reserv;
+    private $_end_date_reserv;
 
     public function getId_reserv(){
         return $this->_id_reserv;
     }
-
-    public function getId_user(){
-        return $this->_id_user;
-    }
-
     public function getId_book(){
         return $this->_id_book;
     }
-
+    public function getId_user(){
+        return $this->_id_user;
+    }
     public function getChange_condition(){
         return $this->_change_condition;
     }
-
     public function getDate_reserv(){
         return $this->_date_reserv;
     }
-
-    public function getDate_limit(){
-        return $this->_date_limit;
+    public function getEnd_date_reserv(){
+        return $this->_end_date_reserv;
     }
 
     public function setId_reserv($id_reserv){
         $this->_id_reserv = $id_reserv;
     }
 
-    public function setId_user($id_user){
-        $this->_id_user = $id_user;
-    }
-
     public function setId_book($id_book){
         $this->_id_book = $id_book;
+    }
+
+    public function setId_user($id_user){
+        $this->_id_user = $id_user;
     }
     
     public function setChange_condition($change_condition){
@@ -49,8 +46,8 @@ class Reserv extends Classes{
         $this->_date_reserv = $date_reserv;
     }
 
-    public function setDate_limit($date_limit){
-        $this->_date_limit = $date_limit;
+    public function setEnd_date_limit($end_date_limit){
+        $this->_end_date_limit = $end_date_limit;
     }
 
 }
