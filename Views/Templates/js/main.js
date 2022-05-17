1f35678783c174baf1fb286b88d606757e1f38be
Alexandre <97739767+Crowny23@users.noreply.github.com>
2022-05-17 10:15:18 +0200

    slider apoint

--- Views/Templates/js/main.js
+++ Views/Templates/js/main.js
@@ -5,8 +5,8 @@ const btncontact = document.querySelector('.btncontact');
 const contact = document.querySelector('.contact');
 const closecontact = document.querySelector('.closec');
 const bookpass = document.getElementsByClassName('imgbooks');
-const arrowleft = document.querySelector('.left');
-const arrowright = document.querySelector('.right');
+const arrowright = document.querySelector('.aleft');
+const arrowleft = document.querySelector('.aright');
 const searchcat = document.getElementById('searchcat');
 const cate = document.getElementById('categories');
 const searchbar = document.querySelector('.inpsearchbar');
@@ -23,9 +23,11 @@ setInterval(() => {
 let nbr = 2;
 let nbrr = 3;
 let nbrl = 1;
+let nbrrh = 4;
+let nbrlh = 0;
 bookpass[nbr].classList.add('act');
-bookpass[nbr-1].classList.add('left');
-bookpass[nbr+1].classList.add('right');
+bookpass[nbrl].classList.add('left');
+bookpass[nbrr].classList.add('right');
 
 for (let i = 0; i < bookpass.length; i++) {
     const element = bookpass[i];
@@ -40,12 +42,48 @@ arrowleft.addEventListener('click', ()=>{
     bookpass[nbr].classList.remove('act');
     bookpass[nbr].classList.add('left');
     bookpass[nbrr].classList.remove('right');
+    bookpass[nbrr].classList.add('act');
+    bookpass[nbrl].classList.remove('left');
+    bookpass[nbrl].classList.add('lhide');
+    bookpass[nbrrh].classList.remove('rhide');
+    bookpass[nbrrh].classList.add('right');
+    nbrl++;
+    nbrr++;
+    nbr++;
+    nbrlh++;
+    nbrrh++;
+    if(nbrl > bookpass.length-1){
+        nbrl = 0;
+    }
+    if(nbr > bookpass.length-1){
+        nbr = 0;
+    }
+    if(nbrr > bookpass.length-1){
+        nbrr = 0;
+    }
+    if(nbrrh > bookpass.length-1){
+        arrowleft.classList.add('pointerNone');
+    }
+    if(nbrlh > bookpass.length-1){
+        nbrlh = 0;
+    }else{
+        arrowright.classList.remove('pointerNone');
+    }
+})
+arrowright.addEventListener('click', ()=>{
+    bookpass[nbr].classList.remove('act');
+    bookpass[nbr].classList.add('right');
+    bookpass[nbrl].classList.remove('left');
+    bookpass[nbrl].classList.add('act');
+    bookpass[nbrr].classList.remove('right');
     bookpass[nbrr].classList.add('rhide');
-    bookpass[nbrl].classList.remove('lhide');
-    bookpass[nbrl].classList.add('left');
+    bookpass[nbrlh].classList.remove('lhide');
+    bookpass[nbrlh].classList.add('left');
     nbrl--;
     nbrr--;
     nbr--;
+    nbrrh--;
+    nbrlh--;
     if(nbr < 0){
         nbr = bookpass.length-1;
     }
@@ -55,28 +93,14 @@ arrowleft.addEventListener('click', ()=>{
     if(nbrl < 0){
         nbrl = bookpass.length-1;
     }
-    bookpass[nbr].classList.add('act');
-})
-arrowright.addEventListener('click', ()=>{
-    bookpass[nbr].classList.remove('act');
-    bookpass[nbr].classList.add('right');
-    bookpass[nbrl].classList.remove('left');
-    bookpass[nbrl].classList.add('lhide');
-    bookpass[nbrr].classList.remove('rhide');
-    bookpass[nbrr].classList.add('right');
-    nbrl++;
-    nbrr++;
-    nbr++;
-    if(nbr > bookpass.length-1){
-        nbr = 0;
+    if(nbrrh < 0){
+        nbrrh = bookpass.length-1;
+    }else{
+        arrowleft.classList.remove('pointerNone');
     }
-    if(nbrr > bookpass.length-1){
-        nbrr = 0;
+    if(nbrlh < 0){
+        arrowright.classList.add('pointerNone');
     }
-    if(nbrl > bookpass.length-1){
-        nbrl = 0;
-    }
-    bookpass[nbr].classList.add('act');
 })
 
 btnmenu.addEventListener('click', ()=>{
