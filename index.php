38425b2948063f1d557ad6e4f8e280d79003d600
Kay'z.Part <particulier.flore@gmail.com>
2022-05-23 08:46:36 +0200

    pagination

--- index.php
+++ index.php
@@ -35,7 +35,6 @@ $router->map('GET', '/spaceAdmin', 'ControllerAdmin#space');
 
 
 // Recherche 
-// $router->map('GET', '/search','ControllerBook#searchBook');
 $router->map('GET', '/search', 'ControllerBook#spaceSearch');
 
 // Réservation livre 
