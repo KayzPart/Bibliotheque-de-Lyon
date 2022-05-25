96c3f0b8ce6227a4bc2ac149a6519f9fc0e83f2c
Alexandre <97739767+Crowny23@users.noreply.github.com>
2022-05-25 10:52:15 +0200

    resolution de probleme avec safari

--- Controllers/ControllerBook.php
+++ Controllers/ControllerBook.php
@@ -18,35 +18,35 @@ class ControllerBook extends ControllerTwig
         $datas = new ModelBook();
         $book = $datas->select($id);
 
-        public static function newBook($datas){
-            $datas = $_POST;
-            $manager = new ModelBook();
-            $newBook = $manager->insertBook($datas);
-            require_once './Views/admin_book_ajout.php';
-        }
-        public static function Show(){
-            session_start();
-            if(!isset($_SESSION['adminId'])){
-                header("Refresh: 0.01; url = ./connectAdmin");
-            }
-            $twig = ControllerTwig::twigcontrol();
-            echo $twig->render('admin_book_ajout.twig', ['root' => ROOT]);
-        }
-        
-        public static function spaceSearch(){
-            $searchcat = $_GET['searchcat'];
-            $p = isset($_GET['p']) ? $_GET['p'] - 1 : 0;
-            if($searchcat == 'id_category'){
-                $search = $_GET['categories'];
-            }else{
-                $search = $_GET['s'];
-            }
-            $twig = Controllertwig::twigcontrol();
-            $manager = new ModelBook();
-            $result = $manager->spaceSearch($searchcat, $search, $p);
-            echo $twig->render('search.twig', ['root' => ROOT, 'books' => $result[0], 'nbrpages' => $result[1]]);
-        }
         echo $twig->render('book.twig', ['book' => $book[0], 'category' => $book[1], 'root' => ROOT]);
+    }
+    public static function newBook($datas){
+        $datas = $_POST;
+        $manager = new ModelBook();
+        $newBook = $manager->insertBook($datas);
+        require_once './Views/admin_book_ajout.php';
+    }
+    public static function Show(){
+        session_start();
+        if(!isset($_SESSION['adminId'])){
+            header("Refresh: 0.01; url = ./connectAdmin");
+        }
+        $twig = ControllerTwig::twigcontrol();
+        echo $twig->render('admin_book_ajout.twig', ['root' => ROOT]);
+    }
+    
+    public static function spaceSearch(){
+        $searchcat = $_GET['searchcat'];
+        $p = isset($_GET['p']) ? $_GET['p'] - 1 : 0;
+        if($searchcat == 'id_category'){
+            $search = $_GET['categories'];
+        }else{
+            $search = $_GET['s'];
+        }
+        $twig = Controllertwig::twigcontrol();
+        $manager = new ModelBook();
+        $result = $manager->spaceSearch($searchcat, $search, $p);
+        echo $twig->render('search.twig', ['root' => ROOT, 'books' => $result[0], 'nbrpages' => $result[1]]);
     }
 
 
